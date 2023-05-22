b715b3e2b21a004c4dcf59086eb3156abcfa657f
Gaurav Yadav <gaurav@kuants.in>
2023-05-22 10:30:51 +0530

    added more features

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,85 +1,105 @@
-import React from 'react'
-import '../header/header.css';
-import CTA from '../cta/CTA';
-import ME from '../../assets/codingguy.png';
-import HeaderSocials from './HeaderSocials';
-import CV from '../../assets/GauravResume.pdf';
-import Typewriter from 'typewriter-effect';
-import Lottie from 'react-lottie';
-import  Frontend from '../../lotties/frontendanimation.json';
-
+import React from "react";
+import "../header/header.css";
+import CTA from "../cta/CTA";
+import ME from "../../assets/codingguy.png";
+import HeaderSocials from "./HeaderSocials";
+import CV from "../../assets/GauravResume.pdf";
+import Typewriter from "typewriter-effect";
+import Lottie from "react-lottie";
+import Frontend from "../../lotties/frontendanimation.json";
+import { useState, useEffect } from "react";
 
 const Header = () => {
+  const [windowSize, setWindowSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
 
+  // we use the useEffect hook to listen to the window resize event
+  useEffect(() => {
+    window.onresize = () => {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+  }, []);
 
-  
   return (
+    <div className="mainCtn">
+      <div className="subCtn">
+        <div className="nameCtn">
+          <div className="headingCtn">
+            <h5 id="helloTag">Hi, my name is </h5>
+            <text id="name">Gaurav Kumar Yadav</text>
+            <h5 id="profession">
+              I'm software engineer specializing in building(and occasionally
+              designing) exceptional digital experiences. Currently I'm focussed
+              on building accessble human-centered products at Kuants Finance
+              Private Limited.
+            </h5>
+            <h3 style={{ color: "blue" }}>
+              Window Width: {windowSize.width}px
+            </h3>
+            <h3 style={{ color: "red" }}>
+              Window Height: {windowSize.height}px
+            </h3>
+          </div>
 
-
-    <div className='mainCtn'>
-
-
-        <div className='subCtn'>
-
-
-            <div className='nameCtn'>
-
-
-                <div className='headingCtn'>
-                    <h5 id='helloTag'>Hi, my name is </h5>
-                    <text id='name'>Gaurav Kumar Yadav</text>
-                    <h5 id='profession'>I'm software engineer specializing in building(and occasionally designing) exceptional digital experiences.
-                    Currently I'm focussed on building accessble human-centered products at Kuants Finance Private Limited.
-                    </h5>
-
-                </div>
-                
-                <div className='typewriterCtn'>
-
-                <Typewriter
-                    options={{
-                      strings: ['Frontend Developer','Backend Developer', 'Fullstack developer'],
-                      autoStart: true,
-                      loop: true,
-                    }}
-                  />
-
-                </div>
-             
-                <CTA/>
-
-                <div style={{ marginTop:"2rem" }}>
-                <Lottie options={{
+          <div className="typewriterCtn">
+            <Typewriter
+              options={{
+                strings: [
+                  "Frontend Developer",
+                  "Backend Developer",
+                  "Fullstack developer",
+                ],
+                autoStart: true,
                 loop: true,
-                autoplay: true, 
+              }}
+            />
+          </div>
+
+          {windowSize.width < 480 ? (
+            <div className="me">
+              <img alt="HAHHA" src={ME}></img>
+            </div>
+          ) : null}
+
+          <CTA />
+
+          <div style={{ marginTop: "2rem" }}>
+            <Lottie
+              options={{
+                loop: true,
+                autoplay: true,
                 animationData: Frontend,
                 rendererSettings: {
-                  preserveAspectRatio: 'xMidYMid slice'
-                }
+                  preserveAspectRatio: "xMidYMid slice",
+                },
               }}
-                        height={250}
-                        width={250}
-                        />
-            </div>
-            </div>
-         
-            <div className='me'>
+              height={250}
+              width={250}
+            />
+          </div>
+        </div>
+
+        {/* <div className='me'>
               <img  alt='HAHHA' src ={ME}></img>
-            </div>
+            </div> */}
 
+        {windowSize.width > 480 ? (
+          <div className="me">
+            <img alt="HAHHA" src={ME}></img>
+          </div>
+        ) : null}
 
-
-            <div id='header-socials'>
-            <HeaderSocials/>
-            </div>
+        <div id="header-socials">
+          <HeaderSocials />
+        </div>
       </div>
-
-
-  
     </div>
+  );
+};
 
-
-  )
-}
-
-export default Header
+export default Header;
